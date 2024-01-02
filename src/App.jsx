7bec3fbca4c9bb42afd2a9d87a4da43bbc05758a
Thiago Prohaska <thiago.prohaska@gmail.com>
2024-01-02 13:17:35 +0100

    display selected project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -46,7 +47,18 @@ function App() {
     });
   }
 
-  let content;
+  function handleSelectProject(projectId) {
+    setProjectsState(prev => {
+      return {
+        ...prev,
+        selectedProjectId: projectId
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(p => p.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onSaveProject={handleAddProject} onCancel={handleCancelProject} />;
@@ -56,7 +68,8 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8" >
-      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId} projects={projectsState.projects} />
       {content}
     </main>
   );
